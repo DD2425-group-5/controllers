65382bd40173aa6b14dfc813865bcccac2273ced
Dmitrij Lioubartsev <dmitrijl@kth.se>
2014-10-20 15:40:27 +0200

    fixed CMake and package. Added tolerances to blobfollower

--- milestone0controller/src/blobfollower.cpp
+++ milestone0controller/src/blobfollower.cpp
@@ -1,36 +1,59 @@
 #include "blobfollower.hpp"
 
-void blobfollower::rgbdCallback(const geometry_msgs::Twist::ConstPtr& msg) 
-{
-	
-};
 
-blobfollower::blobfollower(int argc, char *argv[])
-{
-	ros::init(argc, argv, "follower");	//name of node
-	ros::NodeHandle handle;			//the handle
-	
-	pub_motor = handle.advertise<geometry_msgs::Twist>("/motor_controller/Twist", 1000);
-	sub_rgbd = handle.subscribe("/tmp/rgb", 1000, &blobfollower::rgbdCallback, this);
-	
-	ros::Rate loop_rate(10);	//10 Hz
-	while (ros::ok())			//main loop of this code
-	{
-		/*Todo add code for following the nearest point according to rgbd (missing)*/
-		
-		
-		geometry_msgs::Twist msg;	//for controlling the motor
-		msg.linear.x = 0.0;
-		msg.angular.x = 0.0;
-		pub_motor.publish(msg);		//pub to motor
-		
-		ros::spinOnce();
-		loop_rate.sleep();
-	}
+blobfollower::blobfollower(int argc, char *argv[]) {
+    ros::init(argc, argv, "follower");	//name of node
+    ros::NodeHandle handle;			//the handle
+
+    pub_motor = handle.advertise<geometry_msgs::Twist>("/motor_controller/Twist", 1000);
+    sub_rgbd = handle.subscribe("/vision/closest_blob", 1000, &blobfollower::rgbdCallback, this);
+
+    halfwidth = 640/2;
+    desired_depth = 500;
+
+    width_tolerance = 30;
+    depth_tolerance = 60;
+
+    linear_speed = 0.1;
+    angular_speed = 0.1;
+}
+
+void blobfollower::rgbdCallback(const blobdetection::depth_point::ConstPtr& msg) {
+    x = msg->x;
+    depth = msg->depth;
+}
+
+void blobfollower::update() {
+
+    //calculate speeds
+    float ls = 0;
+    if(depth < desired_depth-depth_tolerance)
+        ls = -linear_speed;
+    else if(depth > desired_depth+depth_tolerance)
+        ls = linear_speed;
+
+    float as = 0;
+    if(x < halfwidth-width_tolerance)
+        as = angular_speed;
+    else if(x > halfwidth+width_tolerance)
+        as = -angular_speed;
+
+    //publish
+    geometry_msgs::Twist msg;	//for controlling the motor
+    msg.linear.x = ls;
+    msg.angular.z = as;
+    pub_motor.publish(msg);		//pub to motor
 };
 
 int main(int argc, char *argv[]) 
 {
 	blobfollower blob(argc,argv);
+    ros::Rate loop_rate(10);
+    while(ros::ok()) {
+        blob.update();
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+
 	return 0;
 }
--- milestone0controller/src/blobfollower.hpp
+++ milestone0controller/src/blobfollower.hpp
@@ -1,16 +1,32 @@
 #include "ros/ros.h"
 #include "geometry_msgs/Twist.h"
+#include <blobdetection/depth_point.h>
 
 
 class blobfollower {
 private:
 	ros::Subscriber sub_rgbd;	//subscribe for depth and angle
 	ros::Publisher pub_motor;	//for the motor
-	
+
+    int x;
+    float depth;
+
+    //desired values
+    int halfwidth;
+    float desired_depth;
+
+    //tolerances
+    int width_tolerance;
+    float depth_tolerance;
+
+    //speeds to move at
+    float linear_speed;
+    float angular_speed;
 	
 	//functions
-	void rgbdCallback(const geometry_msgs::Twist::ConstPtr& msg);
+    void rgbdCallback(const blobdetection::depth_point::ConstPtr& msg);
+
 public:
 	blobfollower(int argc, char *argv[]);	//the main loop
-	
+    void update();
 };
