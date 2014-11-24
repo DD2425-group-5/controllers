05f36b83c4d3302e003487e2dc5d23cafc4c7697
qwoper <christoffer.janson@gmail.com>
2014-11-24 18:47:25 +0100

    Simple_explorer skelleton with basic functionality

--- simple_explorer/src/simple_explorer.cpp
+++ simple_explorer/src/simple_explorer.cpp
@@ -1,14 +1,90 @@
 #include "simple_explorer.hpp"
 
 void simple_explorer::runNode(){
+	ros::Rate loop_rate(hz);	//50 Hz
+	
+	while (ros::ok())			//main loop of this code
+	{
+		//ROS_INFO("STATE = %d", state);
+		if(started){
+			char tmp = currentState();	
+			char tmpState=action(tmp);	//what action shoul be taken given the sensor data
+			//ROS_INFO("STATE = %d TMPSTATE = %d", state,tmpState);
+			if(tmpState!=state){		//has the state changed?
+				prevState=state;
+				state=tmpState;
+				ROS_INFO("STATE = %d", state);
+				statep = states[state];
+			}
+			(this->*statep)();
+		}
+		
+		geometry_msgs::Twist msg;	//for controlling the motor
+		
+		msg.linear.x = v;
+		msg.angular.y = y;
+		msg.angular.z = w;
+		
+		msg.linear.x = 0.0;
+		msg.angular.y = 0.0;
+		msg.angular.z = 0.0;
+		/**/
+		
+		pub_motor.publish(msg);		//pub to motor
+		
+		ros::spinOnce();
+		loop_rate.sleep();
+	}
+}
 
+/*what action should be taken?*/
+char simple_explorer::action(char state){
+	char tmp;
+	tmp = state & 0b00110000;	//apply front masking
+	if(tmp>0){ //are there any obstacle in the way
+		//return DONOTHING;
+	}
+	tmp = state & 0b00000101;	//apply lw masking
+	if(5==tmp){ //do we have a wall on the left
+		return FOLLOW_LEFT_WALL;
+	}
+	tmp = state & 0b00001010;	//apply rw masking
+	if(10==tmp){ //no wall on the left but the right then?
+		return FOLLOW_RIGHT_WALL;
+	}
+	return DONOTHING;
+}
+
+void simple_explorer::followrightwallinit(){
+	statep=&simple_explorer::followrightwall;
+}
+
+void simple_explorer::followrightwall(){
+
+}
+
+void simple_explorer::followleftwallinit(){
+	statep=&simple_explorer::followleftwall;
+}
+
+void simple_explorer::followleftwall(){
+	
+}
+
+void simple_explorer::donothing(){
+	//guess what this does
+	//ROS_INFO("STATE: DO NOTHING");
+	v = 0.0;
+	w = 0.0;
+	y = 0.0;
 }
 
 /*calculates and returns the current state*/
 char simple_explorer::currentState(){
-
-	float registrate[] = {0.30,0.30,0.30,0.30,0.15,0.15};
-
+	//at what distance in meters the sensor readings count as walls
+	float registrate[] = {0.20,0.20,0.20,0.20,0.15,0.15};
+	
+	//format xx(s5)(s4)(s3)(s2)(s1)(s0)
 	char tmp = 0b00000000;
 	int tmp2 = 1;
 	for(int i=0;i<6;i++){
@@ -62,16 +138,57 @@ void simple_explorer::sensorCallback(const ir_sensors::IRDists msg){
 	for(int i=0;i<6;i++){
 		sensor[i]=tmp[i];
 	}
-	if(!started && sensor[0]>0){
+	if(!started /*&& sensor[0]>0*/){
 		started=1;
 	}
+	
+	/*ROS_INFO("sensor distance: 1: [%f] 2: [%f] 3: [%f] 4: [%f] 5: [%f] 6: [%f] \n\n",\
+	sensor[0],\
+	sensor[1],\
+	sensor[2],\
+	sensor[3],\
+	sensor[4],\
+	sensor[5]);
+	/**/
 }
 
 simple_explorer::simple_explorer(int argc, char *argv[]){
 	ros::init(argc, argv, "simple_explorer");	//name of node
 	ros::NodeHandle handle;					//the handle
 	
+	//init variables
 	hz = 50;
+	timer = 0;
+	v = 0.0;
+	w = 0.0;
+	y = 0.0;
+	marchSpeed = 0.25;
+	stop = 0;
+	started=0;
+	change=0;
+	
+	//init state machine
+	states[DONOTHING] = &simple_explorer::donothing;
+	states[FOLLOW_LEFT_WALL] = &simple_explorer::followleftwallinit;
+	states[FOLLOW_RIGHT_WALL] = &simple_explorer::followrightwallinit;
+	states[TURN_LEFT] = &simple_explorer::donothing;
+	states[TURN_RIGHT] = &simple_explorer::donothing;
+	state = DONOTHING;
+	prevState = DONOTHING;
+	statep = &simple_explorer::donothing;
+	
+	//std::string turn_pub_topic;
+    //ROSUtil::getParam(handle, "/topic_list/controller_topics/wallfollower/published/turning_topic", turn_pub_topic);
+	//std::string dist_sub_topic;
+    //ROSUtil::getParam(handle, "/topic_list/hardware_topics/ir_sensors/published/ir_distance_topic", dist_sub_topic);
+	//std::string motor_pub_topic;
+    //ROSUtil::getParam(handle, "/topic_list/controller_topics/motor3/subscribed/twist_topic", motor_pub_topic);
+	
+	pub_motor = handle.advertise<geometry_msgs::Twist>("/motor3/twist", 1000);
+	sub_sensor = handle.subscribe("/ir_sensors/dists", 1000, &simple_explorer::sensorCallback, this);
+	pub_turning = handle.advertise<controller_msgs::Turning>("/controller/turn", 1000);
+	
+	runNode();
 }
 
 int main(int argc, char *argv[]) 
--- simple_explorer/src/simple_explorer.hpp
+++ simple_explorer/src/simple_explorer.hpp
@@ -6,26 +6,34 @@
 #include <sstream>
 #include "controller_msgs/Turning.h"
 
+#define DONOTHING			0
+#define FOLLOW_LEFT_WALL	1
+#define FOLLOW_RIGHT_WALL	2
+#define TURN_LEFT			3
+#define TURN_RIGHT			4
+
 class simple_explorer{
 public:
 	simple_explorer(int argc, char *argv[]);
 
 private:
+	//variables
 	float sensor[6];		//	latest sensor data
 	int started;			//	has all the nodes been started?
 	int timer;				//	for wait()
 	int hz;					//	operating frequency
+	double marchSpeed;		//	default run speed
 	
-	/*double v;				//	velocity
+	double v;				//	velocity
 	double w;				//	angular turn
 	double y;				//	turn degrees
 	int stop;				//	set to one if robot turn or stop
 	char state;				//	current state
 	char prevState;			//	previous state
 	int change;				//	used to register changes in states
-	void (wallfollower::*states[64])();	// state pointers
-	void (wallfollower::*statep)();	//state pointer
-	int stop;*/
+	void (simple_explorer::*states[32])();	// state pointers
+	void (simple_explorer::*statep)();	//state pointer
+	/**/
 	
 	ros::Subscriber sub_sensor;	//sub to get distance values
 	ros::Publisher pub_motor;	//for the motor
@@ -38,4 +46,11 @@ private:
 	int wait();			//call to wait
 	int wait(int ms);	//set wait time in ms
 	char currentState();//returns current state
+	char action(char state); //what action should be taken?
+	
+	void donothing();	//self explainatory
+	void followleftwallinit();
+	void followleftwall();
+	void followrightwallinit();
+	void followrightwall();
 };
